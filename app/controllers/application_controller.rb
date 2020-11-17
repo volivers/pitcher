36a6603589e8eef4bb3960ab4ed841f1916d3801
Madalena Boavida Guerra <69923979+BoavidaGuerra@users.noreply.github.com>
2020-11-17 15:51:26 +0000

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    attributes = %i[first_name last_name account_type photo]
+    attributes = %i[first_name last_name account_type]
     devise_parameter_sanitizer.permit(:sign_up, keys:attributes)
     devise_parameter_sanitizer.permit(:account_update, keys:attributes)
   end
