b16d8ac5ba1be2638965582e8da0792c322639c8
Madalena Boavida Guerra <69923979+BoavidaGuerra@users.noreply.github.com>
2020-11-17 15:51:51 +0000

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :demos, dependent: :destroy
-  has_one_attached :photo
 
   validates :first_name, presence: true
   validates :last_name, presence: true
