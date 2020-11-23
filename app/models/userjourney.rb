1a5df1ce534bd160d1999bf27372e095d4575deb
Madalena Boavida Guerra <madalena.boavida@icloud.com>
2020-11-23 13:14:45 +0000

    added validation on userjourney model > name and persona id must be unique. correction on last commit message: added name column to userjourney table.

--- app/models/userjourney.rb
+++ app/models/userjourney.rb
@@ -2,4 +2,7 @@ class Userjourney < ApplicationRecord
   belongs_to :demo
   belongs_to :persona
   has_many :steps, dependent: :destroy
+
+  validates :name, presence: true
+  validates_uniqueness_of :name, scope: :persona_id
 end
