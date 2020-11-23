607f9bbac5e3ff599c4ae3e00b239fbaa98f50b6
Madalena Boavida Guerra <madalena.boavida@icloud.com>
2020-11-23 12:37:40 +0000

    added basic validation to steps model.

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,7 @@
 class Step < ApplicationRecord
   belongs_to :userjourney
+
+  validates :standpoint, presence: true
+  validates :viewpoint, presence: true
+  validates :actionpoint, presence: true
 end
