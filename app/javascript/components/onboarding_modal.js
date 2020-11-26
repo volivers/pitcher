2a014c6a2d1955fe31d162b9324cd85605769229
Maurizio Ghisolfi <maurizioghisolfi93@gmail.com>
2020-11-26 18:45:12 +0100

    pitch and persona working

--- app/javascript/components/onboarding_modal.js
+++ app/javascript/components/onboarding_modal.js
@@ -4,13 +4,13 @@ const onboardingModal = () => {
   let onboardPitch = document.querySelector('#onboard-pitch');
   let pitchNew = document.querySelector('.pitches.new')
 
-  // Persons
+  // Personas
   let onboardPersona = document.querySelector('#onboard-persona');
   let personaNew = document.querySelector('.personas.new')
 
   // Userjourney
   let onboardUserjourney = document.querySelector('#onboard-userjourney');
-  let userjourneyNew = document.querySelector('.userjourney.new')
+  let userjourneyNew = document.querySelector('.userjourneys.new')
 
 
   // ...
@@ -71,7 +71,7 @@ const onboardingModal = () => {
 
   // persona
 
-  if (personaNew) {
+if (personaNew) {
     // let elements = document.querySelectorAll('textarea')
     // let count = elements.length
 
@@ -80,7 +80,7 @@ const onboardingModal = () => {
     let modalBtn = document.querySelector('#hidden-button')
     modalBtn.click()
 
-    if(onboardPitch) {
+    if(onboardPersona) {
         let step1 = document.querySelector('#step1')
         let step2 = document.querySelector('#step2')
         let step3 = document.querySelector('#step3')
@@ -88,10 +88,10 @@ const onboardingModal = () => {
 
         let steps = [step1, step2, step3, step4]
 
-        let content1 = document.querySelector('#onboard .step1')
-        let content2 = document.querySelector('#onboard .step2')
-        let content3 = document.querySelector('#onboard .step3')
-        let content4 = document.querySelector('#onboard .step4')
+        let content1 = document.querySelector('#onboard-persona .step1')
+        let content2 = document.querySelector('#onboard-persona .step2')
+        let content3 = document.querySelector('#onboard-persona .step3')
+        let content4 = document.querySelector('#onboard-persona .step4')
 
         let contents = [content1, content2, content3, content4]
 
@@ -112,7 +112,7 @@ const onboardingModal = () => {
               }
             })
 
-            // add the 'active classe to the step'
+            // add tthe 'active classe to the step'
             e.target.classList.add('active')
 
           });
@@ -120,58 +120,10 @@ const onboardingModal = () => {
         })
     }
   }
+
 
 
   // userjourney
-  if (userjourneyNew) {
-    // let elements = document.querySelectorAll('textarea')
-    // let count = elements.length
-
-    // -------------
-
-    let modalBtn = document.querySelector('#hidden-button')
-    modalBtn.click()
-
-    if(onboardUserjourney) {
-        let step1 = document.querySelector('#step1')
-        let step2 = document.querySelector('#step2')
-        let step3 = document.querySelector('#step3')
-        let step4 = document.querySelector('#step4')
-
-        let steps = [step1, step2, step3, step4]
-
-        let content1 = document.querySelector('#onboard .step1')
-        let content2 = document.querySelector('#onboard .step2')
-        let content3 = document.querySelector('#onboard .step3')
-        let content4 = document.querySelector('#onboard .step4')
-
-        let contents = [content1, content2, content3, content4]
-
-        steps.forEach((step) => {
-
-          step.addEventListener('click', (e) => {
-            // remove 'active' class from all the steps
-            steps.forEach((step) => step.classList.remove('active'))
-
-            // display the correct one
-            let targetClass = e.currentTarget.id
-
-            // hide all the contetns
-            contents.forEach( (content) => {
-              content.classList.add('d-none')
-              if(content.classList.contains(targetClass)) {
-                content.classList.remove('d-none')
-              }
-            })
-
-            // add the 'active classe to the step'
-            e.target.classList.add('active')
-
-          });
-
-        })
-    }
-  }
 
 
 }
