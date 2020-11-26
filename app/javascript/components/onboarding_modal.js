0ed1cf607e9be93834d24c2fa11be5e55149f343
Maurizio Ghisolfi <maurizioghisolfi93@gmail.com>
2020-11-26 19:14:59 +0100

    working on pitch persona and userjourney

--- app/javascript/components/onboarding_modal.js
+++ app/javascript/components/onboarding_modal.js
@@ -125,6 +125,56 @@ if (personaNew) {
 
   // userjourney
 
+if (userjourneyNew) {
+    // let elements = document.querySelectorAll('textarea')
+    // let count = elements.length
+
+    // -------------
+
+    let modalBtn = document.querySelector('#hidden-button')
+    modalBtn.click()
+
+    if(onboardUserjourney) {
+        let step1 = document.querySelector('#step1')
+        let step2 = document.querySelector('#step2')
+        let step3 = document.querySelector('#step3')
+        let step4 = document.querySelector('#step4')
+
+        let steps = [step1, step2, step3, step4]
+
+        let content1 = document.querySelector('#onboard-userjourney .step1')
+        let content2 = document.querySelector('#onboard-userjourney .step2')
+        let content3 = document.querySelector('#onboard-userjourney .step3')
+        let content4 = document.querySelector('#onboard-userjourney .step4')
+
+        let contents = [content1, content2, content3, content4]
+
+        steps.forEach((step) => {
+
+          step.addEventListener('click', (e) => {
+            // remove 'active' class from all the steps
+            steps.forEach((step) => step.classList.remove('active'))
+
+            // display the correct one
+            let targetClass = e.currentTarget.id
+
+            // hide all the contetns
+            contents.forEach( (content) => {
+              content.classList.add('d-none')
+              if(content.classList.contains(targetClass)) {
+                content.classList.remove('d-none')
+              }
+            })
+
+            // add tthe 'active classe to the step'
+            e.target.classList.add('active')
+
+          });
+
+        })
+    }
+  }
+
 
 }
 
