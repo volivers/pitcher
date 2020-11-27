75a3a2fdb1ab3334e39dae7b59cbe2685acde60a
Madalena Boavida Guerra <madalena.boavida@icloud.com>
2020-11-27 13:25:13 +0000

    home page tweaks

--- app/javascript/components/modal.js
+++ app/javascript/components/modal.js
@@ -1,30 +1,14 @@
 const validFalseDemo = (e) => {
   //let dashboardPage = document.querySelector('.demos.dashboard');
-  let demoModalExists = document.querySelector('#demoModal')
+  let modalExists = document.querySelector('#demoModal')
 
-  if(demoModalExists) {
+  if(modalExists) {
     let demoValid = modalExists.dataset.valid;
     // console.log("HEY!!", demoValid)
     if(demoValid == 'false') {
       let button = document.getElementById('buttonModal');
       button.click();
     }
-
-  }
-  let onboardingModalExists = document.querySelector('#onboarding')
-
-  if(onboardingModalExists) {
-
-    onboardingModalExists.click(function() {
-      console.log("HEY!!", demoValid)
-      //var modalDiv = $('#myModal');
-      this.modal({backdrop: false});
-      console.log("HEY!!", demoValid)
-      // $('.modal-dialog').draggable({
-      //   handle: ".modal-header"
-      // });
-    });
-
   }
 }
 
